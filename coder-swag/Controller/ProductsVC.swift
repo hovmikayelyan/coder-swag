7e91e22bc6385e7d664fb824c67187d19618fdf0
Hovhannes Mikayelyan <hovmikayelyan@gmail.com>
2022-08-16 21:04:35 +0400

    Update and fix project issues

---
+++ coder-swag/Controller/ProductsVC.swift
@@ -0,0 +1,46 @@
+//
+//  ProductsVC.swift
+//  coder-swag
+//
+//  Created by Hovhannes Mikayelyan on 16.08.22.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+
+    private(set) public var products = [Product]()
+    
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productsCollection.delegate = self
+        productsCollection.dataSource = self
+    }
+    
+    func initProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+    }
+}
+
+extension ProductsVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "productCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateView(product: product)
+            return cell
+        }
+        
+        return ProductCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
+            return CGSize(width: ((self.view.frame.size.width/2) - 20), height: 242)
+        }
+}
